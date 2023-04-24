db11163f9040c5866316d0b0df9f9a1ab356d154
siroegu <siro41@hotmail.com>
2023-04-24 11:58:08 +0200

    Tests Start

---
+++ tests/Cart/ProductTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Tests\Cart;
+
+class ProductTest
+{
+
+}
---
+++ tests/Cart/UserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Cart;
+
+use User;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+
+}
